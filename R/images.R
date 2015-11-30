9a00e41c696267c3b2f462043fe24f7f00fa24a3
michbur <michalburdukiewicz@gmail.com>
2015-11-30 15:39:04 +0100

    added images

---
+++ R/images.R
@@ -0,0 +1,10 @@
+#' @name Images
+#' @title Exemplary images
+#' @description Images bla bla
+#' @docType data
+#' @format .png images.
+#' @source Some complicated machine.
+#' @examples 
+#' # read all images
+#' readImage(system.file('images/Well_Slide2_9_APC.png', package='EBImage'))
+NULL
